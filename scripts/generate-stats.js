6abb5731f56a1d60e601db6095d5941e17ce0dba
Rakthesh T G <raktheshtg@gmail.com>
2026-07-18 15:50:14 +0530

    Update generate-stats.js

--- scripts/generate-stats.js
+++ scripts/generate-stats.js
@@ -71,7 +71,7 @@ async function fetchContributions(login, token) {
       Accept: 'application/vnd.github+json',
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
-      'User-Agent': 'RaktheshTG-profile-stats',
+      'User-Agent': 'profile-stats-generator',
       'X-GitHub-Api-Version': '2022-11-28',
     },
     body: JSON.stringify({ query: GRAPHQL, variables: { login } }),
